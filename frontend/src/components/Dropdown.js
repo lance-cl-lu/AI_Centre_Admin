c9385e5e42828e64bd8d138cbf46fb7d93c9c671
JaeggerJose <lms025187@gmail.com>
2023-09-07 11:05:33 +0800

    :tada: improve lab_get_user_data

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import AuthContext from '../context/AuthContext';
 import { useLocation } from 'react-router-dom';
 import TableRowsIcon from '@mui/icons-material/TableRows';
-
+import jwtDecode from "jwt-decode";
 function Dropdown() {
   let { logoutUser, user } = useContext(AuthContext);
   const [click, setClick] = useState(false);
@@ -16,17 +16,21 @@ function Dropdown() {
   useEffect(() => {
       closeMobileMenu();
   }, [location]);
+  const permission = jwt_decode(localStorage.getItem('authToken'))['permission']
 
   return (
     <div className="dropdown">
       <button onClick={handleClick} className="btn btn-secondary dropdown-toggle dropbtn"><TableRowsIcon/></button>
       {click ? (
       <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">  
+      { permission !== 'root' ? 
         <li>
           <Link to="/user" className="dropdown-item" state={{"user": user.username}}>
             Profile
           </Link>
         </li>
+        : null
+      }
         <li>
           <Link to="https://hackmd.io/@lms025187/H1SCqhIR3" className="dropdown-item">
             Help
