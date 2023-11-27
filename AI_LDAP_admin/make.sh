82947797059f083decdb0d4240cb938c8c54f270
JaeggerJose <lms025187@gmail.com>
2023-11-27 18:29:04 +0800

    :update: update configuration file

--- AI_LDAP_admin/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Loop until 'make' command is successful
-while ! make; do
-    echo "Make failed, trying again..."
-    sleep 1  # Wait for 1 second before retrying
-done
-
-echo "Make succeeded."
