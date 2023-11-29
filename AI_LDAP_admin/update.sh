12e386818b9f5df3b01d4373c503063ae396430f
Lance Lu <lance.cl.lu@gmail.com>
2023-11-29 21:39:30 +0800

    Finish Backend/Frontend

--- AI_LDAP_admin/update.sh
+++ AI_LDAP_admin/update.sh
@@ -2,3 +2,9 @@ cp -f ../frontend/build/index.html template/
 rm -rf static/*
 cp -a ../frontend/build/* static/
 rm -f static/index.html
+
+sed -i 's/"\/manifest.json"/"{% static "\/manifest.json" %}"/' template/index.html 
+sed -i 's/"\/static\/js\/main./"{% static "\/static\/js\/main./' template/index.html
+sed -i 's/.js"/.js" %}"/' template/index.html 
+sed -i 's/"\/static\/css\/main./"{% static "\/static\/css\/main./' template/index.html
+sed -i 's/.css"/.css" %}"/' template/index.html 
