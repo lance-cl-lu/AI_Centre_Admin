b1b6f890c5b9343b4a911b9f0783c42b37bc69c8
Lance Lu <lance.cl.lu@gmail.com>
2025-01-17 11:37:46 +0800

    Mail content

--- AI_LDAP_admin/api/views.py
+++ AI_LDAP_admin/api/views.py
@@ -25,7 +25,7 @@ import zipfile
 
 def send_email_gmail(subject, message, destination):
     # First assemble the message
-    msg = MIMEText(message, 'plain')
+    msg = MIMEText(message, 'html')
     msg['Subject'] = subject
 
     # Login and send the message
@@ -571,6 +571,7 @@ def adduser(request):
     firstname = data['first_name']
     lastname = data['last_name']
     password = data['password']
+    password_ori = data['password']
     labname = data['lab']
     email = data['email'].lower()
     cpu_quota = data['cpu_quota']
@@ -628,7 +629,32 @@ def adduser(request):
     UserGPUQuotaType.objects.create(user=user, gpuType=gpu_vendor)
     create_profile(username=username, email=email,cpu=cpu_quota, gpu=gpu_quota, memory=mem_quota, manager=manager)
     
-    send_email_gmail('Introduction', 'Account Created', email)
+    k8s_account = username
+    k8s_password = password_ori
+    k8s_name = firstname + ' ' + lastname
+    email_title = '您的帳號已成功建立'
+    email_body = '<!-- ####### HEY, I AM THE SOURCE EDITOR! #########-->'\
+        '<p><span style="font-weight: 400;">親愛的 ' + k8s_name + ' ，</span></p>'\
+        '<p><span style="font-weight: 400;">您好！</span><span style="font-weight: 400;"><br /></span>'\
+        '<span style="font-weight: 400;">感謝您加入我們的服務，以下是您的帳號資訊：</span></p>' \
+        '<ul>' \
+        '<li style="font-weight: 400;" aria-level="1"><strong>使用者名稱</strong><span style="font-weight: 400;">：</span>'\
+        '<span style="font-weight: 400;">' + k8s_account + '</span></li>' \
+        '<li style="font-weight: 400;" aria-level="1"><strong>密碼</strong>'\
+        '<span style="font-weight: 400;">：</span><span style="font-weight: 400;">' + k8s_password +'</span></li>' \
+        '</ul>' \
+        '<p><span style="font-weight: 400;">為了確保您的帳號安全，請您在首次登入後立即修改密碼。</span></p>' \
+        '<p><span style="font-weight: 400;">我們為您準備了一份詳細的使用手冊，幫助您快速熟悉系統功能，您可以透過以下連結查看：</span>'\
+        '<span style="font-weight: 400;"><br /></span><a href="https://zh.wikipedia.org/zh-tw/%E8%B6%85%E6%96%87%E6%9C%AC%E4%BC%A0%E8%BE%93%E5%8D%8F%E8%AE%AE">'\
+        '<span style="font-weight: 400;">點擊這裡下載使用手冊</span></a></p>' \
+        '<p><span style="font-weight: 400;">如果您在使用過程中遇到任何問題，歡迎隨時聯繫我們的客服團隊，我們將竭誠為您服務。</span></p>' \
+        '<p><strong>&nbsp;</strong></p>' \
+        '<p><span style="font-weight: 400;">祝您使用愉快！</span></p>' \
+        '<p><strong><br /><span style="font-weight: 400;">此致</span>'\
+        '<span style="font-weight: 400;"><br /></span><span style="font-weight: 400;">長庚大學 AI 中心</span>'\
+        '<span style="font-weight: 400;"><br /></span><span style="font-weight: 400;">aiplatform@cgu.edu.tw</span></strong></p>'
+
+    send_email_gmail(email_title, email_body, email)
 
     return Response(status=200)
 
